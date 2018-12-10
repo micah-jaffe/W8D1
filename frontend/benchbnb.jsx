27c1dd8a4b9c2d15b9159b1bbbfac723ab6b7a00
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-10 09:51:29 -0800

    Setup entry point

---
+++ frontend/benchbnb.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // configure store
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>hello</h1>, root);
+})
